640f91f8da722a85af403e81192a55a9bafb9ff2
Yuon <tobiasmeier204@gmail.com>
2024-01-24 22:33:53 +0100

    Commit

---
+++ how2writefiles/branch.py
@@ -0,0 +1,10 @@
+
+def leadr(self, x, y):
+        lrbd = lrbd[(1, x, y),
+                    (2, x, y),
+                    (3, x, y),
+                    (4, x, y),
+                    (5, x, y),]
+        
+        print(lrbd)
+
--- number-guess/number-guess-game.py
+++ number-guess/number-guess-game.py
@@ -1,35 +1,49 @@
 # I want to create a game where the user has to guess a randomly generated number within a specified range.
 # add leaderboard, high-score system
-# add condition if first try print different message,
 
 import random
 
-counter = 0
+tries = 1
+pre_tries = 0
+highest_tries = 0
+
 x = True
 
 while x == True:
     rng = random.randint(1,10)
     unum = None
     try:
-        unum = (input("Guess the number between 1 and 10: "))
-
-        if unum == int:
-            print("The correct guess was "+str(rng))
-            print("You guessed "+str(unum))
-            if unum == rng:
-                if counter == 0:
-                    print("You guessed it on your first try!")
-                else:
-                    print("You guessed the right number")
-                    print("It took you "+str(counter)+" times to figure the correct number!")
-                x = False
+        unum = int(input("Guess the number between 1 and 10: "))
+        
+        print("The correct guess was "+str(rng))
+        print("You guessed "+str(unum))
+        if unum == rng:
+            if tries == 0:
+                print("You guessed it on your first try!")
             else:
-                print("You guessed the wrong number!")
-                counter += 1
+                print("You guessed the right number")
+                print("It took you "+str(tries)+" times to figure the correct number!")
+            if tries >= highest_tries:
+                highest_tries = tries
+            print("Current Amount: "+str(tries))
+            print("Previous Amount: "+str(pre_tries))
+            print("Highest Amount: "+str(highest_tries))
+            pre_tries = tries
+            while True:
+                play_again = input("Would you like to play again? (y/n): ")
+                if play_again.lower == "n":
+                    x = False
+                elif play_again.lower == "y":
+                    pass
+                
         else:
-            print("Error:")
-            print("")
-            print("Please enter a Number / (Integer)")
-            print("")
+            print("You guessed the wrong number!")
+            tries += 1
+
+    except ValueError as e:
+        print("Error:")
+        print("")
+        print("Please enter a Number / (Integer)")
+        print(e)
     finally:
         pass
