d0fe1684adb8832bdbcd2f2eae4ffa9d3d9f79c1
Yuon <tobiasmeier204@gmail.com>
2024-01-25 21:58:29 +0100

    modified

--- passwort_gen/pw_gen.py
+++ passwort_gen/pw_gen.py
@@ -1,18 +1,26 @@
 # Build a program that generates strong and secure passwords based on user-provided preferences.
 
+'''info
+>>>def random_char(y):
+       return ''.join(random.choice(string.ascii_letters) for x in range(y))
+
+>>>print (random_char(5))
+>>>fxkea
+'''
+
 import random
+import string
+
+string.ascii_letters
 
 class user_pref:
-    upref = str(input("Enter a password you like to strenghen: "))
-    upref = upref.lower()
-
+      upref = str(input("Enter a password you like to strenghen: ")).lower()
 class strmnpl(user_pref): #string manipulation
-    uprefrnd = random.randint(0,15)
-    
+    uprefrnd = random.choice(string.ascii_letters)
     print(uprefrnd)
-
-
+    for i in range(1, 4):
+        x = random.randint(1,15)
+        y = random.randint(1,15)
+        print(x,y)
 def again(): # play again
     pass
-
-print(user_pref())
