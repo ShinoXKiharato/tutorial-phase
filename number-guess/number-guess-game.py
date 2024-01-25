e3f6357f463c4c46c0069990ad8a40a8c3af6bb3
Yuon <tobiasmeier204@gmail.com>
2024-01-25 21:00:43 +0100

    working game

--- number-guess/number-guess-game.py
+++ number-guess/number-guess-game.py
@@ -1,11 +1,17 @@
 # I want to create a game where the user has to guess a randomly generated number within a specified range.
-# add leaderboard, high-score system
 
 import random
 
 tries = 1
 pre_tries = 0
 highest_tries = 0
+
+def play_againQ():
+    play_again = input("Would you like to play again? (y/n): ").upper()
+    if play_again == "N":
+        quit()
+    else:
+        return False
 
 x = True
 
@@ -15,30 +21,28 @@ while x == True:
     try:
         unum = int(input("Guess the number between 1 and 10: "))
         
-        print("The correct guess was "+str(rng))
-        print("You guessed "+str(unum))
+        print("The correct guess was: "+str(rng))
+        print("You guessed: "+str(unum))
         if unum == rng:
-            if tries == 0:
+            if tries == 1:
                 print("You guessed it on your first try!")
             else:
                 print("You guessed the right number")
                 print("It took you "+str(tries)+" times to figure the correct number!")
+            print("")
             if tries >= highest_tries:
                 highest_tries = tries
             print("Current Amount: "+str(tries))
             print("Previous Amount: "+str(pre_tries))
             print("Highest Amount: "+str(highest_tries))
             pre_tries = tries
-#add variable Set true, change while v=true 
-            while True:
-                play_again = input("Would you like to play again? (y/n): ")
-                if play_again.lower == "n":
-                    x = False
-                elif play_again.lower == "y":
-                    pass
-                
+            print("")
+            while play_againQ():
+                break
+            tries = 1
         else:
             print("You guessed the wrong number!")
+            print("")
             tries += 1
 
     except ValueError as e:
