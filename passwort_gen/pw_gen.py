dbca3a3c801e3669589ff2d08a350e0b7b2acc40
Yuon <tobiasmeier204@gmail.com>
2024-01-25 21:07:46 +0100

    NGG .V1 + New Prjct
    Number Guess game version 1 finished
    
    added dir for new project called
    passwort generator.

---
+++ passwort_gen/pw_gen.py
@@ -0,0 +1,11 @@
+# Build a program that generates strong and secure passwords based on user-provided preferences.
+
+def user_pref():
+    pass
+
+def strmnpl(): #string manipulation
+    pass
+
+def again():
+    pass
+
