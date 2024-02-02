5274e146cd86132b250f5881bcd72e348e7c4b6d
Yuon <tobiasmeier204@gmail.com>
2024-02-02 13:03:32 +0100

    update

--- passwort_gen/pw_gen.py
+++ passwort_gen/pw_gen.py
@@ -13,14 +13,18 @@ numpy.random.choice to give a unique set if you add replace=False,
 
 like so: numpy.random.choice(string.ascii_lowercase, size=5, replace=False)
 '''
-
+# imp note join()
+# imp note lstrip() to remove smth from string
+# imp note replace() to replace a substring
+# imp note swapcase() swaps all case letters big to low, low to big
+# imp lookup .sample()
 import random
 import string
 
 string.ascii_letters
 
 class user_pref:
-      upref = str(input("Enter a password you like to strenghen: ")).lower()
+      upref = str(input("Enter a password you like to strenghen: ")).lower().isalnum()
 class strmnpl(user_pref): #string manipulation
     uprefrnd = random.choice(string.ascii_letters)
     print(uprefrnd)
