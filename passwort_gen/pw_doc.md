a24cbba936c8d566619f4055bc01b11916c04aad
Yuon <tobiasmeier204@gmail.com>
2024-02-07 13:15:26 +0100

    snap

--- passwort_gen/pw_doc.md
+++ passwort_gen/pw_doc.md
@@ -8,11 +8,25 @@ Correleated to the Python programm PW_Gen.py
 
 - [ ] (Optional) Maybe add according to input instead of set integers in line 58. 'if list == result'
 
-- [ ] Add back if statement for 3 duplicate letters behind each other / lenght is < 3.
-
 - [ ] Make Each password look individual af.
 
 - [ ] (Optional) Add numbers and more to the generator.
 
 ---
 ## Code
+
+First Attempt for no duplicates.
+```py
+
+if u_input[::-1].lower() in result_str[-len_u_input::-1].lower() or u_input[-3::-1].lower() in result_str[-3::-1].lower():
+                while rng_letter != result_str[-1:]:
+                    rng_letter = random.choice(string.ascii_letters)
+                    if rng_letter == result_str[-1:]:
+                        break
+                result_str = str(rng_letter)
+                    
+            elif rng_list_letter.lower() in result_str[-3::-1].lower() or rng_list_letter.lower() in result_str[-2:].lower(): 
+                result_str = str(rng_letter)
+            else:
+                result_str = str(rng_list_letter) 
+```
--- passwort_gen/pw_gen.py
+++ passwort_gen/pw_gen.py
@@ -1,29 +1,20 @@
-# Build a program that generates strong and secure passwords based on user-provided preferences.
-
-# imp note join()
-# imp note lstrip() to remove smth from string
-# imp note replace() to replace a substring
-# imp note swapcase() swaps all case letters big to low, low to big
-# imp lookup .sample() ¦ isalpnum()
 import random
 import string
 
 string.ascii_letters   
 
 
-def space(**we):
-     print_rslt =  ""
-     for r in we.values():
-         print_rslt += r
-         print(print_rslt)
-     return print_rslt
+def space(strings):
+     for string in strings:
+         print(string, end="")
+     print()
 
 display=("")
 
 def again(): # play again
     play_again = input("Again (Y/N)?: ").upper()
     if play_again == 'Y':
-        return display(e="")
+        display("")
     else:
         quit()
 
@@ -31,41 +22,31 @@ display = space
 p = True
 
 while p == True:
-    gen_pw = ""
-    pw_lenght = int(input("Set new passwort lenght: "))
-    u_input = input("Enter a password you like to strenghen: ")
-    display(k='------------')
-    ''' optional, modify later
-    if len(u_input) <= 3 or u_input[1] == u_input[2:3]: # <----------
-        while len(gen_pw) < pw_lenght:
-            x = random.choice(string.ascii_letters)
-            print(x,end="")
+    generated_password = ""
+    password_lenght = int(input("Set new passwort lenght: "))
+    user_input = input("Enter a password you like to strenghen: ")
+    len_u_input = len(user_input)
 
-            gen_pw += x'''
+    display('-' * 10)
 
 
-    if len(u_input) >= 1:
-        while len(gen_pw) < pw_lenght:
+    while len(generated_password) < password_lenght:
 
-            rng_letter = random.choice(string.ascii_letters) 
-            rng_int_input = random.randint(1, 3 or 4)
-            rng_int_input2 = lambda rng_int_input:random.randint(1, 6) if len(u_input) >= 5 else None
-            rng_char_input = u_input[::rng_int_input or rng_int_input2]
+        rng_letter = random.choice(string.ascii_letters) 
+        rng_int_input = random.randint(1, 6) if len_u_input >= 5 else random.randint(1, (3 or 4))
+        rng_char_input = user_input[::rng_int_input]
 
-            li_st = [rng_letter, rng_char_input, rng_letter, rng_letter, rng_char_input]
-            rng_list_letter = random.choice(li_st)
-            result_str = "" + rng_list_letter
+        next_char = random.choice([rng_letter, rng_char_input])
+        result_str = '' + next_char
 
-            if rng_list_letter == result_str[-1 or -2::2 and 3 and 4]: 
-                result_str = str(rng_letter)
-            else:
-                result_str = str(rng_list_letter) 
-            
-            print(str(result_str), end="")
+        
+        
+        
+        print(str(result_str), end="")
 
-            gen_pw += result_str
+        generated_password += result_str
 
-    display(k="")
-    print(len(gen_pw), "Characters")
-    display(k='------------')
+    display('')
+    display([len(generated_password), " Characters"])
+    display('-'*10)
     again()
