d0c78cb7f1feb4780d0c5a43231836fbd3cb1ec2
Yuon <tobiasmeier204@gmail.com>
2024-02-07 12:59:00 +0100

    Update
    Improved readability and code.
    
    removed optional (for now)
    
    removed checker, have to work on this one.

--- passwort_gen/pw_gen.py
+++ passwort_gen/pw_gen.py
@@ -23,6 +23,7 @@ import string
 
 string.ascii_letters
 
+<<<<<<< Updated upstream
 class user_pref:
       upref = str(input("Enter a password you like to strenghen: "))
 # count lenght of user string and use lenght to create a random number int from 1 to x(how long the userstring is)
@@ -44,6 +45,20 @@ def again(): # play again
     x = input("Again (Y/N)?: ").upper()
     if x == 'Y':
         return None
+=======
+
+def space(strings):
+     for string in strings:
+         print(string, end="")
+     print()
+
+display=("")
+
+def again(): # play again
+    play_again = input("Again (Y/N)?: ").upper()
+    if play_again == 'Y':
+        display("")
+>>>>>>> Stashed changes
     else:
         quit()
 
@@ -61,8 +76,17 @@ p = True
 rslt = 0
 # if int(len(rslt)) != 10:
 while p == True:
+<<<<<<< Updated upstream
     s(k='------------')
     for i in range(1, 10):
+=======
+    gen_pw = ""
+    pw_lenght = int(input("Set new passwort lenght: "))
+    u_input = input("Enter a password you like to strenghen: ")
+    len_u_input = len(u_input)
+
+    display('-' * 10)
+>>>>>>> Stashed changes
 
         zin = random.choice(string.ascii_letters)
         op = random.randint(1, 3)
@@ -77,7 +101,37 @@ while p == True:
             x = random.choice(string.ascii_letters)
             #y = random.randint(1,15)
             print(x,end="")
+<<<<<<< Updated upstream
             x += x
     s(k="")
     s(k='------------')
+=======
+
+            gen_pw += x'''
+
+
+    if len(u_input) >= 1:
+        while len(gen_pw) < pw_lenght:
+
+            rng_letter = random.choice(string.ascii_letters) 
+            rng_int_input = random.randint(1, 3 or 4)
+            rng_int_input2 = lambda rng_int_input:random.randint(1, 6) if len_u_input >= 5 else False
+            rng_char_input = u_input[::rng_int_input or rng_int_input2]
+
+            li_st = [rng_letter, rng_char_input, rng_letter, rng_letter, rng_char_input]
+            rng_list_letter = random.choice(li_st)
+            result_str = "" + rng_list_letter
+
+
+            
+            
+            
+            print(str(result_str), end="")
+
+            gen_pw += result_str
+
+    display('')
+    display([len(gen_pw), "Characters"])
+    display('-'*10)
+>>>>>>> Stashed changes
     again()
