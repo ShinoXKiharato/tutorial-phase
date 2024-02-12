f8aa73e1d7966923073c736aedb389b9f0cf9d98
Yuon <tobiasmeier204@gmail.com>
2024-02-12 13:02:55 +0100

    updates

--- key_input/empty.py
+++
@@ -1,9 +0,0 @@
-# Future project use
-
-from pyKey import pressKey, releaseKey, press, sendSequence, showKeys
-from time import sleep
-
-sleep(1)
-sendSequence('Hello World ! :3')
-press('ENTER')
-showKeys()
